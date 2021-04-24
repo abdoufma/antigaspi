ab709691b72c03967006de6bfe721da119bf3042
Abdou <gh.abds@gmail.com>
2021-04-24 15:32:01 +0100

    fixed remote uploads

--- upload.js
+++ upload.js
@@ -4,39 +4,39 @@ const {join, extname, basename} = require('path');
 const multer = require('multer');
 const {randomBytes} = require('crypto');
 
-const sharp = require('sharp');
+// const sharp = require('sharp');
 const upload_dir = join(__dirname,'public/images/uploads/');
 const thumbs_dir = join(__dirname,'public/images/thumbs/');
 
-const save_thumbnail = true;
+const save_thumbnail = false;
 
 function MyCustomStorage ({destination}) {
   this.getDestination = destination;
 }
 
-MyCustomStorage.prototype._handleFile = function _handleFile (req, file, cb) {
-  this.getDestination(req, file, async function (err, path) {
-    if (err) return cb(err)
+// MyCustomStorage.prototype._handleFile = function _handleFile (req, file, cb) {
+//   this.getDestination(req, file, async function (err, path) {
+//     if (err) return cb(err)
 
-    let outStream = fs.createWriteStream(path);
-    file.stream.pipe(outStream);      
+//     let outStream = fs.createWriteStream(path);
+//     file.stream.pipe(outStream);      
 
-    outStream.on('error', cb);
-    outStream.on('finish',  async () => {
-        try{
-            let filename = basename(path);     
-            await sharp(path).resize({height:250 }).toFile(thumbs_dir+filename);
-            cb(null, {path, size: outStream.bytesWritten});
-        }catch(e){
-            console.log(e);
-        }
-    });
-  })
-}
+//     outStream.on('error', cb);
+//     outStream.on('finish',  async () => {
+//         try{
+//             let filename = basename(path);     
+//             await sharp(path).resize({height:250 }).toFile(thumbs_dir+filename);
+//             cb(null, {path, size: outStream.bytesWritten});
+//         }catch(e){
+//             console.log(e);
+//         }
+//     });
+//   })
+// }
 
-MyCustomStorage.prototype._removeFile = function _removeFile (req, file, cb) {
-  fs.unlink(file.path, cb);
-}
+// MyCustomStorage.prototype._removeFile = function _removeFile (req, file, cb) {
+//   fs.unlink(file.path, cb);
+// }
 
 const generate_filename = (file) => randomBytes(24).toString('hex') + extname(file.originalname);
 
